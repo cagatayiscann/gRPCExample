227340a8f53b1b639ecfb400f490a8c8ad5be241
cagatayiscann <cagatayiscan@gmail.com>
2026-02-07 17:02:56 +0300

    unary example

--- grpcClient/Program.cs
+++ grpcClient/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Grpc.Net.Client;
+using grpcMessageClient;
+using grpcServer;
+
+namespace grpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("http://localhost:5296");
+            // var greetClient = new Greeter.GreeterClient(channel);
+
+            // HelloReply result = await greetClient.SayHelloAsync(new HelloRequest{
+            //     Name = "Çağatay'dan selamlar"
+            // });
+
+            var messageClient = new Message.MessageClient(channel);
+
+            MessageResponse response = await messageClient.SendMessageAsync(new MessageRequest
+            {
+                Message = "Çağatay efendinin hürmetleri",
+                Name = "Çağatay'ın habercisi"
+            });
+
+            System.Console.WriteLine(response.Message);
+        }
+    }
+}
